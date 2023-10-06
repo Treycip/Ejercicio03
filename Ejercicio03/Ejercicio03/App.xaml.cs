971bbc937b1702cec32ef5b98ce6fedceb00ff84
treycip <treyci.perez@tecsup.edu.pe>
2023-10-06 15:17:04 -0500

    Commit

--- Ejercicio03/Ejercicio03/App.xaml.cs
+++ Ejercicio03/Ejercicio03/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Ejercicio03
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
